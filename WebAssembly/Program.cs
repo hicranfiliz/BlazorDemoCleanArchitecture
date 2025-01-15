db5b1d5b8abaeb8170d3296af59aba8cfc2647c4
hicranfiliz <h.filiz781@gmail.com>
2025-01-15 10:28:00 +0300

    added employee from web home page

--- WebAssembly/Program.cs
+++ WebAssembly/Program.cs
@@ -1,13 +1,16 @@
 using ApplicationLayer.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WebAssembly;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7177/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7187/") });
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
